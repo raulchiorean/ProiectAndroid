e6ddeaeefb24545277e08d3c1f382e76db166e37
weakness100 <raul_chiorean97@yahoo.ro>
2017-09-15 17:11:44 +0300

    1

--- TheApp/app/src/main/java/com/example/nesty/theapp/Meniu.java
+++ TheApp/app/src/main/java/com/example/nesty/theapp/Meniu.java
@@ -1,13 +1,17 @@
 package com.example.nesty.theapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+
 public class Meniu extends AppCompatActivity {
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +22,22 @@ public class Meniu extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file =getFile();
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
                 startActivityForResult(intent,0);
             }
         });
+    }
+    private File getFile(){
+        File folder = new File("sdcard/camera_app");
+        if(!folder.exists())
+        {
+            folder.mkdir();
+
+        }
+        File image_file = new File (folder,i+"cam_image.jpg");
+        i++;
+        return image_file;
     }
 
 }
