12f2acf218e4505738c403c05aa8a01e12d58a8b
weakness100 <raul_chiorean97@yahoo.ro>
2017-09-21 14:24:08 +0300

    3

--- TheApp/app/src/main/java/com/example/nesty/theapp/Meniu.java
+++ TheApp/app/src/main/java/com/example/nesty/theapp/Meniu.java
@@ -5,53 +5,202 @@ import android.net.Uri;
 import android.os.Environment;
 import android.os.StrictMode;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import io.fotoapparat.Fotoapparat;
+import io.fotoapparat.FotoapparatSwitcher;
+import io.fotoapparat.error.CameraErrorCallback;
+import io.fotoapparat.hardware.CameraException;
+import io.fotoapparat.parameter.LensPosition;
+import io.fotoapparat.parameter.ScaleType;
+import io.fotoapparat.parameter.update.UpdateRequest;
+import io.fotoapparat.photo.BitmapPhoto;
+import io.fotoapparat.preview.Frame;
+import io.fotoapparat.preview.FrameProcessor;
+import io.fotoapparat.result.PendingResult;
+import io.fotoapparat.result.PhotoResult;
+import io.fotoapparat.view.CameraView;
+
+import static io.fotoapparat.log.Loggers.fileLogger;
+import static io.fotoapparat.log.Loggers.logcat;
+import static io.fotoapparat.log.Loggers.loggers;
+import static io.fotoapparat.parameter.selector.AspectRatioSelectors.standardRatio;
+import static io.fotoapparat.parameter.selector.FlashSelectors.autoFlash;
+import static io.fotoapparat.parameter.selector.FlashSelectors.autoRedEye;
+import static io.fotoapparat.parameter.selector.FlashSelectors.off;
+import static io.fotoapparat.parameter.selector.FlashSelectors.torch;
+import static io.fotoapparat.parameter.selector.FocusModeSelectors.autoFocus;
+import static io.fotoapparat.parameter.selector.FocusModeSelectors.continuousFocus;
+import static io.fotoapparat.parameter.selector.FocusModeSelectors.fixed;
+import static io.fotoapparat.parameter.selector.LensPositionSelectors.lensPosition;
+import static io.fotoapparat.parameter.selector.Selectors.firstAvailable;
+import static io.fotoapparat.parameter.selector.SizeSelectors.biggestSize;
+import static io.fotoapparat.result.transformer.SizeTransformers.scaled;
+
 public class Meniu extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_meniu);
-        Button opCamera= (Button) findViewById(R.id.openCamera);//am creat button
-        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();//In this way the VM ignores the file URI exposure.
-        StrictMode.setVmPolicy(builder.build());// pe scurt, ma lasa sca folosesc Uri pentru versiuni mai noi de 23API
-        opCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);//invoke camera
-                //File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES); //salveaza pe sdcard in directoru Pictures
-                File file =getFile();
-               // String pictureName = getPicture();// folosim getPicture ca sa creem un nume unic pentru poza
-               // File imageFile = new File(pictureDirectory,pictureName);//creeaza un path
-                Uri pictureUri1 = Uri.fromFile(file);// convert to Uri
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, pictureUri1);// semna pentru ca vrem sa salvam imaginea
-                startActivityForResult(intent,0);//porneste aplicatia
+
+        private final PermissionsDelegate permissionsDelegate = new PermissionsDelegate(this);
+        private boolean hasCameraPermission;
+        private CameraView cameraView;
+
+        private FotoapparatSwitcher fotoapparatSwitcher;
+        private Fotoapparat frontFotoapparat;
+        private Fotoapparat backFotoapparat;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_meniu);
+
+            cameraView = (CameraView) findViewById(R.id.camera_view);
+            hasCameraPermission = permissionsDelegate.hasCameraPermission();
+
+            if (hasCameraPermission) {
+                cameraView.setVisibility(View.VISIBLE);
+            } else {
+                permissionsDelegate.requestCameraPermission();
             }
 
-            private String getPicture() { // genereaza un nume unic
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss"); // data si ora
-                String timestamp = sdf.format(new Date());
-                return timestamp +".jpg";
-            }
-            private File getFile(){ // creeaza un folder
-                File folder = new File("sdcard/camera_app");
-                if(!folder.exists()) //creeaza folderu daca nu exista
-                {
-                    folder.mkdir();
+            setupFotoapparat();
+
+            takePictureOnClick();
+            focusOnLongClick();
+        }
+
+        private void setupFotoapparat() {
+            frontFotoapparat = createFotoapparat(LensPosition.FRONT);
+            backFotoapparat = createFotoapparat(LensPosition.BACK);
+            fotoapparatSwitcher = FotoapparatSwitcher.withDefault(backFotoapparat);
+        }
+
+
+
+
+
+
+
+        private void focusOnLongClick() {
+            cameraView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    fotoapparatSwitcher.getCurrentFotoapparat().autoFocus();
+
+                    return true;
                 }
-                File image_file = new File (folder,getPicture());//creeaza un path
-                return image_file;
-            }
-        });
+            });
+        }
+
+        private void takePictureOnClick() {
+            cameraView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    takePicture();
+                }
+            });
+        }
+
+        private boolean canSwitchCameras() {
+            return frontFotoapparat.isAvailable() == backFotoapparat.isAvailable();
+        }
+
+        private Fotoapparat createFotoapparat(LensPosition position) {
+            return Fotoapparat
+                    .with(this)
+                    .into(cameraView)
+                    .previewScaleType(ScaleType.CENTER_CROP)
+                    .photoSize(standardRatio(biggestSize()))
+                    .lensPosition(lensPosition(position))
+                    .focusMode(firstAvailable(
+                            continuousFocus(),
+                            autoFocus(),
+                            fixed()
+                    ))
+                    .flash(firstAvailable(
+                            autoRedEye(),
+                            autoFlash(),
+                            torch(),
+                            off()
+                    ))
+                    .frameProcessor(new SampleFrameProcessor())
+                    .logger(loggers(
+                            logcat(),
+                            fileLogger(this)
+                    ))
+                    .cameraErrorCallback(new CameraErrorCallback() {
+                        @Override
+                        public void onError(CameraException e) {
+                            Toast.makeText(Meniu.this, e.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .build();
+        }
+    private String getPicture() { // genereaza un nume unic
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss"); // data si ora
+        String timestamp = sdf.format(new Date());
+        return timestamp +".jpg";
     }
+
+        private void takePicture() {
+            PhotoResult photoResult = fotoapparatSwitcher.getCurrentFotoapparat().takePicture();
+
+            photoResult.saveToFile(new File(getExternalFilesDir("cam_app"), getPicture()));
+
+
+        }
+
+
+
+        @Override
+        protected void onStart() {
+            super.onStart();
+            if (hasCameraPermission) {
+                fotoapparatSwitcher.start();
+            }
+        }
+
+        @Override
+        protected void onStop() {
+            super.onStop();
+            if (hasCameraPermission) {
+                fotoapparatSwitcher.stop();
+            }
+        }
+
+        @Override
+        public void onRequestPermissionsResult(int requestCode,
+                                               @NonNull String[] permissions,
+                                               @NonNull int[] grantResults) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            if (permissionsDelegate.resultGranted(requestCode, permissions, grantResults)) {
+                fotoapparatSwitcher.start();
+                cameraView.setVisibility(View.VISIBLE);
+            }
+        }
+
+        private class SampleFrameProcessor implements FrameProcessor {
+
+            @Override
+            public void processFrame(Frame frame) {
+                // Perform frame processing, if needed
+            }
+
+        }
+
+
 
 
 }
